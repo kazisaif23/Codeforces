9bb1f278b4a314ae71bb5cd264908e856c81def0
Kazi Saif Hossain Emon <133599101+kazisaif23@users.noreply.github.com>
2025-08-02 20:46:11 +0600

    2130/problem/A

---
+++ A_Submission_is_All_You_Need.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastread() (ios_base::sync_with_stdio(false), cin.tie(NULL));
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    ll sum = 0;
+    vi vrr(n);
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        sum += max(a, 1);
+    }
+    cout << sum << endl;
+}
+
+int main()
+{
+    fastread();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
