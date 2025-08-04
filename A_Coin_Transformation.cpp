9e9829c2e309131646b8264b586ad9c73d8989d5
Kazi Saif Hossain Emon <133599101+kazisaif23@users.noreply.github.com>
2025-08-04 08:23:44 +0600

    2043/problem/A

---
+++ A_Coin_Transformation.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastread() (ios_base::sync_with_stdio(false), cin.tie(NULL));
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll coin = 1;
+    while (1)
+    {
+        if (n < 4)
+        {
+            break;
+        }
+        n /= 4;
+        coin *= 2;
+    }
+    cout << coin << endl;
+}
+
+int main()
+{
+    fastread();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
