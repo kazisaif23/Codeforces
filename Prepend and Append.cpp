a4014b91b7450e1fa53a784cd0f549547eed055d
Kazi Saif Hossain Emon <133599101+kazisaif23@users.noreply.github.com>
2025-09-19 21:44:18 +0600

    Create Prepend and Append.cpp
    https://codeforces.com/problemset/problem/1791/C

---
+++ Prepend and Append.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastread() (ios_base::sync_with_stdio(false), cin.tie(NULL));
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string bs;
+    cin >> bs;
+    ll sl = n;
+    ll l = 0, r = n - 1;
+    while (l <= r)
+    {
+        if (bs[l] != bs[r])
+        {
+            sl -= 2;
+        }
+        else
+            break;
+        l++;
+        r--;
+        /* code */
+    }
+    cout << sl << endl;
+}
+
+int main()
+{
+    fastread();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
