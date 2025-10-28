1c2a27a9c763e2a5d0b258051058ff407e7ea184
Kazi Saif Hossain Emon <133599101+kazisaif23@users.noreply.github.com>
2025-10-28 09:58:30 +0600

    Create A_CPT.cpp

---
+++ A_CPT.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+
+void solve() {
+    
+}
+
+int main() {
+    fastread();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+    solve();
+     }
+    return 0;
+}
